874cfeaff87cf14e3f62122ffe0c41a08dd5f048
Wesley Dias <wdias@acesso.com>
2020-09-28 08:57:36 -0300

    Update mail.js

--- src/config/mail.js
+++ src/config/mail.js
@@ -2,9 +2,13 @@ export default {
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
     secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
+    },
+    tls: {
+        ciphers: 'SSLv3',
     },
     default: {
         from: 'Equipe Projeto <noreply@projeto.com>',
